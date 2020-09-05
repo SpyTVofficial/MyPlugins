55c03e8da4e2a45b3e29f7a9cf98266cb123da75
Spy <imorlux@icloud.com>
2020-09-05 20:13:57 +0200

    'Commit'

---
+++ serverstats/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<?php require_once('../assets/assets.php'); ?>
+
+<html>
+
+    <head>
+
+        <title> <?php echo htmlspecialchars($title); ?> Server stats</title>
+
+    </head>
+
+    <body>
+        <div class=Stats>
+        <p1>Online Players: </p1>
+        <p2>Online Staff Members: </p2>
+        <p3>Currently Banned Players: </p3>
+        <p4>Curently Muted Players: </p4>
+        </div>
+    </body>
+
+</html>
