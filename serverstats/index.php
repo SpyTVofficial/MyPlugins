a6cedb91f8d28bc8d6292cb6a76efc0d5ade1b4b
SpyTVofficial <imorlux@gmail.com>
2020-09-05 22:19:17 +0200

    'style_css_broken'

--- serverstats/index.php
+++ serverstats/index.php
@@ -12,10 +12,12 @@
 
     <body>
         <div class=Stats>
+        <span>
             <p1>Online Players: </p1>
             <p2>Online Staff Members: </p2>
             <p3>Currently Banned Players: </p3>
             <p4>Curently Muted Players: </p4>
+        </span>
         </div>
     </body>
 
