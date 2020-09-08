2357cd765afd4957741ff9967843d3c0a6daa301
itsSpyTV <imorlux@icloud.com>
2020-09-08 13:35:07 +0200

    User Management

---
+++ adminpanel/manage/users/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<?php require_once('assets/assets.php'); ?>
+<html>
+    
+    <head>    
+        <link rel="stylesheet" href="../../../assets/style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <title> <?php echo htmlspecialchars($title); ?> User Management </title>
+    </head>
+
+    <body>
+
+
+<div class="topnav">
+  <a href="index.php">Home</a>
+  <a href="plugins/index.php">Plugins</a>
+  <a href="contact/index.php">Contact</a>
+  <a href="signin/index.php">Sign in</a>
+  <a href="serverstats/index.php">Serverstats</a>
+  <a href="adminpanel/index.php">Adminpanel</a>
+</div>
+
+<div class="management_user">
+  <p> ... </p>
+</div>
+
+
+<div class="footer">
+  <p><a href="contact/index.php">Contact </a> | MyPlugins | &#169 2020 | <a href="faq/index.php">FAQ</a> | Zu <a href="lsg/index.php">LSG</a></p>
+</div>
+
+</body>
+</html>
