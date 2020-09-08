7689f0f0c4712d52af50822e238585cef2c491b1
itsSpyTV <imorlux@icloud.com>
2020-09-08 12:48:41 +0200

    'Title'

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <head>    
         <link rel="stylesheet" href="assets/style.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <title> <?php echo htmlspecialchars($title); ?> Home </title>
+        <title> <?php echo htmlspecialchars($title); ?> Homepage </title>
     </head>
 
     <body>
